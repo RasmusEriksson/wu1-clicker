39a2cfca111bb3d0289c4412d4c5d71da899d654
RasmusEriksson <rasmus.eriksson@elev.ga.ntig.se>
2025-04-10 10:31:24 +0200

    la till en ny upgradering + lite mer design

--- js/grave.js
+++ js/grave.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('#grave_clicker')
 const counter = document.querySelector('#souls')
+const sps_tracker = document.querySelector('#sps_tracker')
 const upgrade_menu = document.querySelector('#upgrades_menu')
 
 const upgrade_template = document.querySelector('#u_template')
@@ -11,7 +12,7 @@ let soulsPerSecond = 0
 
 let upgrades = [
     {
-        name: "Skeletony",
+        name: "Skeleton",
         description: "One of your creations helps you summon more souls, * +2 souls per second *",
         img: "Skeleton",
         cost: 20,
@@ -26,11 +27,20 @@ let upgrades = [
     },
     {
         name: "Cursed Totem",
-        description: "It feels cursed with dark power, it gives you strength, *+2 souls per click*",
+        description: "It feels cursed with dark power, it gives you strength, *+4 souls per click*",
         img: "Totem",
         cost: 550,
-        SPC: 2,
+        SPC: 4,
         scaling: 1.5
+    },
+    {
+        name: "Reaper",
+        description: "Death itself, its empowered by your undead, *+7 souls per second for each skeleton you have*",
+        img: "Totem",
+        cost: 1666,
+        SPS: 7,
+        Dependancy: "Skeleton",
+        scaling: 1.75
     }
 ];
 
@@ -50,11 +60,6 @@ function create_upgrade(upgrade) {
     const upgrade_button = children.item(1).childNodes.item(5)
     const img = children.item(3)
     
-    if (upgrade.scaling) {
-        scaling = upgrade.scaling
-    }
-
-
     img.id = upgrade.img
 
     upgrade_button.textContent = `(${amount}) Cost: ${cost}`
@@ -62,24 +67,55 @@ function create_upgrade(upgrade) {
     active_upgrades[upgrade.name] = {
         up_cost: cost,
         up_amount: amount,
+        up_SPS: 0,
         button: upgrade_button
+    }
+
+    if (upgrade.SPS) {
+        active_upgrades[upgrade.name]["SPS_increase"] = upgrade.SPS
+    }
+
+    if (upgrade.scaling) {
+        scaling = upgrade.scaling
+    }
+    if (upgrade.Dependancy) {
+        for (const[other_name,other_upgrade] of Object.entries(active_upgrades)) {
+            if (other_name === upgrade.Dependancy) {
+                active_upgrades[other_name]["affiliate"] = upgrade.name
+            }
+        }
     }
 
     upgrade_button.addEventListener(
         'click', (event) => {
             if (souls >= cost) {
-
+                
                 souls -= cost
+                amount += 1
+                active_upgrades[upgrade.name].up_amount = amount
+
                 if (upgrade.SPS) {
-                    soulsPerSecond += upgrade.SPS
+                    if (active_upgrades[upgrade.name].affiliate) {
+                        var affiliate = active_upgrades[upgrade.name].affiliate
+                        active_upgrades[affiliate].up_SPS = amount * active_upgrades[affiliate].SPS_increase * active_upgrades[affiliate].up_amount
+                    }
+                    
+                    if (upgrade.Dependancy) {
+                        active_upgrades[upgrade.name].up_SPS = amount * active_upgrades[upgrade.name].SPS_increase * active_upgrades[upgrade.Dependancy].up_amount
+                    }
+                    else {
+                        active_upgrades[upgrade.name].up_SPS += active_upgrades[upgrade.name].SPS_increase
+                    }
+                    
                 }
                 if (upgrade.SPC) {
                     soulPerClick += upgrade.SPC
                 }
 
-                amount += 1
+                
                 cost = Math.round(upgrade.cost * Math.pow(scaling,amount))
                 active_upgrades[upgrade.name].up_cost = cost
+                
 
                 upgrade_button.textContent = `(${amount}) Cost: ${cost}`
             }
@@ -105,13 +141,17 @@ button.addEventListener(
 let last = 0
 
 function step(timestamp) {
-    counter.textContent = souls
+    var increase = 0
+
     if (timestamp >= last + 1000) {
-        souls += soulsPerSecond
+        for (const [name, upgrade] of Object.entries(active_upgrades)) {
+            souls += upgrade.up_SPS
+        }
         last = timestamp
     }
 
     for (const [name, upgrade] of Object.entries(active_upgrades)) {
+        increase += upgrade.up_SPS
         if (upgrade.up_cost > souls) {
             if (upgrade.button.classList.contains("button_off") !== true) {
                 upgrade.button.classList.add("button_off")
@@ -125,8 +165,9 @@ function step(timestamp) {
             }
         };
       }
+    sps_tracker.textContent = increase
 
-
+    counter.textContent = souls
     window.requestAnimationFrame(step);
 };
 
